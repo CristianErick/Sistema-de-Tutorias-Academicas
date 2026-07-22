8e4b6efb7a894cfae54fd3abd8370314a43010c6
Cris <cris@tutorias.com>
2026-07-22 11:50:50 -0500

    Debug: log de conexion BD (URL mascarada)

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,12 @@
 const { Pool } = require('pg');
 const logger = require('../utils/logger');
 
+const dbUrl = process.env.DATABASE_URL;
+const maskedUrl = dbUrl ? dbUrl.replace(/\/\/[^:]+:[^@]+@/, '//***:***@') : 'NO SET';
+logger.info(`Conectando a BD: ${maskedUrl}`);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: dbUrl,
   max: 3,
   idleTimeoutMillis: 5000,
   connectionTimeoutMillis: 20000,
@@ -10,8 +14,7 @@ const pool = new Pool({
   family: 4,
 });
 
-pool.on('error', (err) => {
-  logger.error('Error en pool PostgreSQL:', err.message);
-});
+pool.on('connect', () => logger.info('Pool conectado a BD'));
+pool.on('error', (err) => logger.error('Error en pool PostgreSQL:', err.message));
 
 module.exports = pool;
